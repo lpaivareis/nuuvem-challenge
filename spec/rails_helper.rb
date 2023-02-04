aea1c22c1ebbf59b870707439eb16380f40d325d
Lucas Felipe <lucasfelipedepaiva2@gmail.com>
2023-02-04 18:03:11 -0300

    Fix shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Shoulda::Matchers::ActiveModel
+  config.include Shoulda::Matchers::ActiveRecord
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
