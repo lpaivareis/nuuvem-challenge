450135eea61ecbd01ddbed9277b6d77cd456660f
Lucas Felipe <lucasfelipedepaiva2@gmail.com>
2023-02-04 17:36:16 -0300

    Add Order files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_173941) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_203355) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_173941) do
 
   create_table "import_files", force: :cascade do |t|
     t.float "total_amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "item_description"
+    t.string "item_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
